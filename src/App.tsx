f06a91822b947bc2645630620c989fa3f5451d23
kkata <14849535+kkata@users.noreply.github.com>
2022-09-09 20:37:03 +0900

    19 20 confirm button disable on checkbox check

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,32 @@ import { useState } from "react";
 
 export const App = () => {
   const [buttonColor, setButtonColor] = useState("red");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
   const handleClick = () => {
     setButtonColor(newButtonColor);
   };
+
+  const handleCheck = () => {
+    setButtonDisabled(!buttonDisabled);
+  };
+
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={handleClick}>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={handleClick}
+        disabled={buttonDisabled}
+      >
         Change to {newButtonColor}
       </button>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        onClick={handleCheck}
+        defaultChecked={buttonDisabled}
+        aria-checked={buttonDisabled}
+      />
     </div>
   );
 };
