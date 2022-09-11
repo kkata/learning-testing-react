5c90af77f0e58e06c170ae3d92e25f7c0a3dfc94
kkata <14849535+kkata@users.noreply.github.com>
2022-09-11 21:56:01 +0900

    all the rest of sec04

--- sundaes-on-demand/src/pages/summary/SummaryForm.tsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.tsx
@@ -1,16 +1,46 @@
 import { useState } from "react";
+import { Button, Form, OverlayTrigger, Popover } from "react-bootstrap";
 
 export const SummaryForm = () => {
-  const [isChecked, setIsChecked] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
+  const checkboxLabel = (
+    <span>
+      I agree to{" "}
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
+
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
   return (
-    <div>
-      <input type="checkbox" onClick={handleCheckboxChange} />
-      <button type="button" disabled={isChecked}>
-        Confirm Order
-      </button>
-    </div>
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!isChecked}>
+        Confirm order
+      </Button>
+    </Form>
+    // <div>
+    //   <input type="checkbox" onClick={handleCheckboxChange} />
+    //   <button type="button" disabled={isChecked}>
+    //     Confirm Order
+    //   </button>
+    // </div>
   );
 };
--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.tsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.tsx
@@ -1,27 +1,54 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { SummaryForm } from "../SummaryForm";
 
 test("Checkbox is unchecked and button is disabled by default", () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox");
-  const button = screen.getByRole("button");
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const button = screen.getByRole("button", { name: /confirm order/i });
 
   expect(checkbox).not.toBeChecked();
   expect(button).toBeDisabled();
 });
 
-test("Checking checkbox enables button and Unchecking checkbox again disables button", () => {
+test("Checking checkbox enables button and Unchecking checkbox again disables button", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
 
-  const checkbox = screen.getByRole("checkbox");
-  const button = screen.getByRole("button");
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const button = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
-  expect(checkbox).toBeChecked();
+  await user.click(checkbox);
   expect(button).toBeEnabled();
 
-  fireEvent.click(checkbox);
-  expect(checkbox).not.toBeChecked();
+  await user.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+test("Popover responds to hover", async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  const nullPopover2 = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover2).not.toBeInTheDocument();
 });
