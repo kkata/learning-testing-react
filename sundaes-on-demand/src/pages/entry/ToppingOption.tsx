16264e01277c4f89f78a04c362653bc936052ac3
kkata <14849535+kkata@users.noreply.github.com>
2022-09-16 20:34:40 +0900

    75. react: toppins checkboxes

--- sundaes-on-demand/src/pages/entry/ToppingOption.tsx
+++ sundaes-on-demand/src/pages/entry/ToppingOption.tsx
@@ -1,11 +1,17 @@
+import { Form } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 
 type PropsType = {
   name: string;
   imagePath: string;
+  updateItemCount: (itemName: string, newItemCount: string) => void;
 };
 
-export const ToppingOption = ({ name, imagePath }: PropsType) => {
+export const ToppingOption = ({
+  name,
+  imagePath,
+  updateItemCount,
+}: PropsType) => {
   return (
     <Col xs={12} sm={6} md={4} style={{ textAlign: "center" }}>
       <img
@@ -13,6 +19,15 @@ export const ToppingOption = ({ name, imagePath }: PropsType) => {
         alt={`${name} topping`}
         style={{ width: "75%" }}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check
+          type="checkbox"
+          onChange={(e) => {
+            updateItemCount(name, e.target.checked ? "1" : "0");
+          }}
+          label={name}
+        />
+      </Form.Group>
     </Col>
   );
 };
