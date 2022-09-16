96e84454b9cc7ca7f57ecfa5c9047ead041bfa3a
kkata <14849535+kkata@users.noreply.github.com>
2022-09-16 20:11:30 +0900

    testing update toppings subtotal
    maybe use constant:
    https://stackoverflow.com/questions/3360074/do-you-use-constants-from-the-implementation-in-your-test-cases

--- sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.tsx
+++ sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.tsx
@@ -26,3 +26,31 @@ test("update scoop subtotal when scoops change", async () => {
   await userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtotal when toppings change", async () => {
+  render(<Options options="toppings" />);
+
+  // make sure total starts out $0.00
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // update cherries toppings to 1 and check the subtotal
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await userEvent.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // update hot fudge toppings to 2 and check the subtotal
+  const hotFudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  await userEvent.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  // uncheck cherries and check the subtotal
+  await userEvent.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+});
