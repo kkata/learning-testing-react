c98afe4b866e21fb6d4a25456dc723c5b488e5f2
kkata <14849535+kkata@users.noreply.github.com>
2022-09-18 23:16:09 +0900

    78. unmouted component error

--- sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.tsx
+++ sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.tsx
@@ -57,19 +57,15 @@ test("update toppings subtotal when toppings change", async () => {
 });
 
 describe("grand total", () => {
-  test("grand total starts at $0.00", () => {
-    render(<OrderEntry />);
-    const grandTotal = screen.getByRole("heading", {
-      name: /grand total: \$/i,
-    });
-    expect(grandTotal).toHaveTextContent("0.00");
-  });
-
   test("grand total updates properly if scoop is added first", async () => {
     render(<OrderEntry />);
     const grandTotal = screen.getByRole("heading", {
       name: /grand total: \$/i,
     });
+
+    // check that the grand total starts out at $0.00
+    expect(grandTotal).toHaveTextContent("0.00");
+
     const vanillaInput = await screen.findByRole("spinbutton", {
       name: "Vanilla",
     });
